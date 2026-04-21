e8ce026d460521b8c95136e3b95e3c00ce193d29
rokman123 <rokismank@gmail.com>
2026-04-21 20:00:39 +0300

    followup

---
+++ Migrations/20260421164733_rm-qstCatEnum.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace math4ktu_be.Migrations
+{
+    /// <inheritdoc />
+    public partial class rmqstCatEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.DropColumn(
+                name: "QuestionCategoryClass",
+                table: "Questions");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionCategoryClass",
+                table: "Questions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
